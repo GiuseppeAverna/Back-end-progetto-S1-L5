5d6252b7fd13771aa0cdc71ff58a5e2254a83277
GiuseppeAverna <151162626+GiuseppeAverna@users.noreply.github.com>
2024-03-08 16:53:12 +0100

    implementazione metodo scelta con switch case

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,40 @@
+import java.util.Scanner;
+
 public class Main {
+
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
          Audio traccia1 = new Audio("Highway to hell", 4, 5);
-        traccia1.alzaVolume();
-         traccia1.play();
          Video filmato = new Video("Italian spiderman", 7, 6, 8  );
-         filmato.abbassaVolume();
-         filmato.alzaLuminosità();
-         filmato.play();
          Immagine foto = new Immagine("immagine.img",5);
-         foto.alzaLuminosità();
-         foto.show();
+        System.out.println("inserisci un numero da 1 a 5( 4 e 5 non ancora disponibili)");
+        int elemento = scanner.nextInt();
+        scanner.nextLine();
+        switch (elemento) {
+            case 1: traccia1.alzaVolume();
+                    traccia1.play();
+
+            break;
+            case 2:  filmato.abbassaVolume();
+                     filmato.alzaLuminosità();
+                     filmato.play();
+            break;
+            case 3:   foto.alzaLuminosità();
+                      foto.show();
+            break;
+            default:
+                System.out.print("Elemento non trovato");
+        }
+
+
+
 
 
 
 
     }
+
+
 
 
 
